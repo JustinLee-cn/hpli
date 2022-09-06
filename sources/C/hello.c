8054023d1c6f9be21f0f244642928fc1db4f3202
hpli <1968263451@qq.com>
2022-09-06 15:32:13 +0800

    没啥东西

--- sources/C/hello.c
+++ sources/C/hello.c
@@ -1,3 +1,11 @@
+/*
+ * @Date: 2022-08-29 17:29:23
+ * @LastEditors: hpli
+ * @LastEditTime: 2022-09-02 15:50:17
+ * @FilePath: \hpli\sources\C\hello.c
+ * @Author: hpli
+ * @Description: file content
+ */
 #include <stdio.h>
 
 void main()
